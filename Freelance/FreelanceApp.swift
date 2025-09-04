61ef18f7296b205e3fc8e685299fe3fb5d34e3e0
Shahin shokoui <shahin@shahin.de>
2025-09-04 19:23:23 +0200

    Initial Commit

---
+++ Freelance/FreelanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreelanceApp.swift
+//  Freelance
+//
+//  Created by Shahin on 04.09.25.
+//
+
+import SwiftUI
+
+@main
+struct FreelanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FreelanceTests/FreelanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  FreelanceTests.swift
+//  FreelanceTests
+//
+//  Created by Shahin on 04.09.25.
+//
+
+import Testing
+@testable import Freelance
+
+struct FreelanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
