a694b8d280f624c0aec89af8d15981aa05ab2954
Shahin shokoui <shahin@shahin.de>
2025-10-28 22:06:47 +0100

    New LG theme with background

---
+++ Freelance/LiquidGlassUI.swift
@@ -0,0 +1,18 @@
+//
+//  LiquidGlassUI.swift
+//  Freelance
+//
+//  Created by Shahin on 28.10.25.
+//
+
+import SwiftUI
+
+struct LiquidGlassUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LiquidGlassUI()
+}
